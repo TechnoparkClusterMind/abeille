dc73cecf3679b56772f71ca904edd482b6a57f5d
Nikita Zheleztsov <nikita.zheleztsov@mail.ru>
2021-12-08 16:44:21 +0300

    Refactor config dir & raft config example

---
+++ raft_node/config/include/config.hpp
@@ -0,0 +1,43 @@
+#ifndef ABEILLE_CONFIG_H
+#define ABEILLE_CONFIG_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "types.hpp"
+
+namespace abeille {
+
+class Config {
+ public:
+  typedef std::vector<std::string> AddressContainer;
+
+  Config() = default;
+  Config(Config &&conf) = default;
+  Config &operator=(const Config &) = default;
+  Config &operator=(Config &&) = default;
+  ~Config() = default;
+
+  bool is_ok() const noexcept;
+  void parse(std::ifstream& file);
+
+  uint64_t GetId() const noexcept { return id_; }
+  std::string GetUserAddress() const noexcept { return user_address_; }
+  std::string GetRaftAddress() const noexcept { return raft_address_; }
+  std::string GetWorkerAddress() const noexcept { return worker_address_; }
+  const AddressContainer &GetPeers() const noexcept { return peers_; }
+  const AddressContainer &GetWorkers() const noexcept { return workers_; }
+
+ private:
+  AddressContainer peers_;
+  AddressContainer workers_;
+  std::string user_address_;
+  std::string raft_address_;
+  std::string worker_address_;
+  ServerId id_ = 0;
+};
+
+}  // namespace abeille
+
+#endif  // ABEILLE_CONFIG_H
---
+++ raft_node/config/src/config.cpp
@@ -0,0 +1,47 @@
+#include "config.hpp"
+
+#include <google/protobuf/util/json_util.h>
+
+#include "abeille.grpc.pb.h"
+#include "constants.hpp"
+#include "convert.hpp"
+#include "logger.hpp"
+
+using namespace google::protobuf;
+using namespace google::protobuf::util;
+
+namespace abeille {
+
+bool Config::is_ok() const noexcept {
+  return id_ != 0 && !user_address_.empty() && !raft_address_.empty() &&
+         !worker_address_.empty();
+}
+
+void Config::parse(std::ifstream &file) {
+  util::JsonParseOptions opt;
+  opt.ignore_unknown_fields = true;
+  opt.case_insensitive_enum_parsing = false;
+
+  std::string file_data = std::string(std::istream_iterator<char>(file),
+                                      std::istream_iterator<char>());
+
+  RaftConfig proto_config;
+  auto status = JsonStringToMessage(file_data, &proto_config, opt);
+  if (!status.ok()) {
+    LOG_ERROR(status.message().ToString().c_str());
+    return;
+  }
+
+  user_address_ = proto_config.user_address();
+  raft_address_ = proto_config.raft_address();
+  worker_address_ = proto_config.worker_address();
+
+  id_ = address2uint(raft_address_);
+
+  auto peers = proto_config.peers();
+  auto workers = proto_config.workers();
+  peers_ = std::move(std::vector(peers.begin(), peers.end()));
+  workers_ = std::move(std::vector(workers.begin(), workers.end()));
+}
+
+}  // namespace abeille
