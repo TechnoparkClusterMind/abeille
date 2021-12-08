f8c03528b4994b4adb34f2980370824e5a92595a
senago <mirzoyan.oganes@mail.ru>
2021-12-08 13:44:56 +0300

    Refactor user CLI

--- user/cmd/main.cpp
+++ user/cmd/main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "cli.hpp"
+#include "commands.hpp"
 #include "constants.hpp"
 #include "convert.hpp"
 #include "linenoise.hpp"
@@ -17,51 +18,9 @@ using abeille::user::Client;
 
 // ud user/data/test.json
 
-// FIXME: make a way to pass here all addresses to raft nodes
-Client user_client({abeille::USER_SERVICE_ADDRESS});
-
-std::string Exit(CLI::args_type) { throw std::exception(); }
-
-std::string UploadData(CLI::args_type args) {
-  if (args.size() != 2) {
-    return "error: invalid usage";
-  }
-
-  std::string filepath = args[1];
-  std::ifstream file(filepath);
-  if (!file.good()) {
-    LOG_TRACE();
-    return "failed to open the file";
-  }
-
-  std::stringstream input;
-  input << file.rdbuf();
-
-  auto task = new TaskData();
-  auto status = google::protobuf::util::JsonStringToMessage(input.str(), task);
-  if (!status.ok()) {
-    return status.message().ToString();
-  }
-
-  auto response = user_client.UploadData(task);
-
-  std::stringstream output;
-  output << "task id is [" << response.task_id() << "]";
-
-  return output.str();
-}
-
 void RunCLI() {
-  const CLI::handlers_map handlers = {{"exit", Exit}, {"ud", UploadData}};
-
-  const CLI::handlers_helper helper = {
-      {"exit", "gracefully terminates the programm"},
-      {"ud <filepath>",
-       "uploads data from the <filepath> to abeille (json format with "
-       "accordance to the proto file)"},
-  };
-
-  CLI cli(handlers, helper);
+  abeille::user::commands::Wrapper wrapper;
+  CLI cli(wrapper.handlers, wrapper.helper);
 
   linenoise::SetMultiLine(true);
   linenoise::SetHistoryMaxLen(4);
---
+++ user/include/commands.hpp
@@ -0,0 +1,35 @@
+#ifndef ABEILLE_USER_COMMANDS_HPP_
+#define ABEILLE_USER_COMMANDS_HPP_
+
+#include "abeille.grpc.pb.h"
+#include "cli.hpp"
+#include "constants.hpp"
+#include "user_client.hpp"
+
+using abeille::cli::CLI;
+using abeille::user::Client;
+
+namespace abeille {
+namespace user {
+namespace commands {
+
+struct Wrapper {
+  Wrapper() noexcept;
+  CLI::handlers_map handlers;
+  CLI::handlers_helper helper;
+};
+
+// FIXME: make a way to pass here all addresses to raft nodes
+static Client user_client({abeille::USER_SERVICE_ADDRESS});
+
+std::string Exit(CLI::args_type);
+
+std::string UploadData(CLI::args_type args);
+
+std::string Status(CLI::args_type args);
+
+}  // namespace commands
+}  // namespace user
+}  // namespace abeille
+
+#endif  // ABEILLE_USER_COMMANDS_HPP_
